c45620393ea269dc13c08e5addd16b77df9a9b05
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2019-04-16 14:38:30 +0200

    Added exceptions

---
+++ dftlib/exceptions/exceptions.py
@@ -0,0 +1,12 @@
+class DftTypeNotKnownException(Exception):
+    """
+    Exception when the type of the DFT element is not known
+    """
+    pass
+
+
+class DftTypeNotSupportedException(Exception):
+    """
+    Exception when the type of the DFT element is not supported
+    """
+    pass
