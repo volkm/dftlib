161d941e5f853883ff0f3ba4611a4356d85052ad
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2019-04-16 14:40:53 +0200

    Support for trimming unused elements

--- bin/simplify.py
+++ bin/simplify.py
@@ -3,6 +3,7 @@ import argparse
 import dftlib.io.parser
 import dftlib.io.export
 import dftlib.transformer.rewriting
+import dftlib.transformer.trimming
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simplify a DFT by rewriting.')
@@ -19,9 +20,18 @@ if __name__ == "__main__":
     # Split FDEPs in DFT
     dftlib.transformer.rewriting.split_fdeps(dft)
 
-    # Simplify DFT
-    dftlib.transformer.rewriting.simplify_dft(dft)
-    print(dft)
+    changed = True
+    while changed:
+        no_elements_before = len(dft.elements)
+        # Simplify DFT
+        dftlib.transformer.rewriting.simplify_dft(dft, rules=["1", "2", "5"])
+        print(dft)
+
+        dftlib.transformer.trimming.trim(dft)
+        print(dft)
+
+        no_elements_after = len(dft.elements)
+        changed = (no_elements_before != no_elements_after)
 
     # Save DFT again
     dftlib.io.export.export_dft_json(dft, args.out)
---
+++ dftlib/transformer/trimming.py
@@ -0,0 +1,42 @@
+def trim(dft):
+    '''
+    Trim parts of the DFT which do not contribute to the top level element.
+    :param dft: DFT.
+    :return: DFT without unnecessary parts.
+    '''
+
+    # List of unused elements to trim
+    unused = {elem_id: element for elem_id, element in dft.elements.items()}
+    visited = set()
+
+    # Perform breadth-first search to find all necessary elements
+    queue = [dft.top_level_element]
+    while len(queue) > 0:
+        current = queue[0]
+        visited.add(current)
+        queue = queue[1:]
+
+        if current.element_id in unused:
+            # Element is necessary
+            del unused[current.element_id]
+
+        # Continue search
+        if current.is_be():
+            # Add possible SEQ or FDEP gates
+            for parent in current.ingoing:
+                if parent.element_type == "fdep" or parent.element_type == "seq" or parent.element_type == "pdep":
+                    if parent not in visited:
+                        queue.append(parent)
+        elif current.is_gate():
+            # Add children of gate
+            for child in current.outgoing:
+                if child not in visited:
+                    queue.append(child)
+
+    assert len(unused) == len(dft.elements) - len(visited)
+
+    # Remove unused elements
+    print("Removing {} unnecessary elements.".format(len(unused)))
+
+    for element_id, element in unused.items():
+        dft.remove(element)
