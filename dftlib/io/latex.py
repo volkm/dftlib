b288c5e0aeaeb3964f4b1fd529de605848107eb5
Matthias Volk <m.volk@utwente.nl>
2022-07-22 12:42:57 +0200

    Support constant BE in Latex export

--- dftlib/io/latex.py
+++ dftlib/io/latex.py
@@ -1,3 +1,4 @@
+import dftlib.storage.dft_be as dft_be
 import dftlib.storage.dft_gates as dft_gates
 from dftlib.exceptions.exceptions import DftTypeNotKnownException, DftTypeNotSupportedException
 
@@ -18,7 +19,10 @@ def generate_tikz_node(element, is_tle=False):
             no_children = str(len(element.outgoing))
 
     if element.is_be():
-        node_type = "be"
+        if isinstance(element, dft_be.BeConstant):
+            node_type = "cnst"
+        else:
+            node_type = "be"
     elif isinstance(element, dft_gates.DftAnd) or isinstance(element, dft_gates.DftVotingGate) or isinstance(element, dft_gates.DftPand):
         node_type = "and" + no_children
     elif isinstance(element, dft_gates.DftOr) or isinstance(element, dft_gates.DftPor):
@@ -38,6 +42,8 @@ def generate_tikz_node(element, is_tle=False):
     label_node = ""
     if isinstance(element, dft_gates.DftVotingGate):
         label_node = "\\rotatebox{{270}}{{${}$}}".format(element.voting_threshold)
+    elif isinstance(element, dft_be.BeConstant):
+        label_node = "$\\top$" if element.failed else "$\\bot$"
     s += "\t\\node[{}] ({}) at {} {{{}}};\n".format(node_type, name, position, label_node)
 
     # Add triangles for PAND or POR
@@ -58,9 +64,23 @@ def generate_tikz_node(element, is_tle=False):
     s += "\t\\node[labelbox] ({0}_label) at ({0}.{1}) {{{2}}};\n".format(name, label_anchor, label)
 
     # Add ratebox for BEs
-    if element.is_be() and element.rate > 0:
-        label = "\\ratelabel{{{0}}}{{{1}}}{{{2}}}".format(name, element.rate, element.rate * element.dorm)
-        s += "\t\\node[ratebox] ({0}_rate) at ({0}.south) {{{1}}};\n".format(name, label)
+    if element.is_be():
+        if isinstance(element, dft_be.BeExponential):
+            if element.dorm < 1:
+                label = "\\ratelabel{{{0}}}{{{1}}}".format(element.rate, element.rate * element.dorm)
+            else:
+                label = "\\rateactlabel{{{0}}}".format(element.rate)
+            s += "\t\\node[ratebox] ({0}_rate) at ({0}.south) {{{1}}};\n".format(name, label)
+        elif isinstance(element, dft_be.BeProbability):
+            if element.dorm < 1:
+                label = "\\problabel{{{0}}}{{{1}}}".format(element.probability, element.probability * element.dorm)
+            else:
+                label = "\\probactlabel{{{0}}}".format(element.probability)
+            s += "\t\\node[ratebox] ({0}_prob) at ({0}.south) {{{1}}};\n".format(name, label)
+        elif isinstance(element, dft_be.BeConstant):
+            pass
+        else:
+            raise DftTypeNotSupportedException("BE distribution '{}' not supported for tikz export.".format(element.distribution))
 
     # Add probability for PDEP
     if isinstance(element, dft_gates.DftDependency) and element.probability < 1:
