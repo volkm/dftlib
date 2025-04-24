91f3ed46b1a856a63de0ae248b44c27e13250fdd
Matthias Volk <m.volk@tue.nl>
2025-04-24 18:08:57 +0200

    Do not replace FDEP by OR if it leads to a cycle

--- tests/transformer/test_rewrite_rules.py
+++ tests/transformer/test_rewrite_rules.py
@@ -172,6 +172,24 @@ def test_rewrite_all_rule28():
     assert no_elements == 4
 
 
+def test_rewrite_all_fdep_cycle():
+    file = get_example_path("simplify", "fdep_cycle.json")
+    dft = dftlib.io.parser.parse_dft_json_file(file)
+    no_be, no_static, no_dynamic, no_elements = dft.statistics()
+    assert no_be == 4
+    assert no_static == 3
+    assert no_dynamic == 2
+    assert no_elements == 9
+
+    changed = simplifier.simplify_dft_all_rules(dft)
+    assert changed
+    no_be, no_static, no_dynamic, no_elements = dft.statistics()
+    assert no_be == 4
+    assert no_static == 3
+    assert no_dynamic == 1
+    assert no_elements == 8
+
+
 def test_merge_bes_parametric():
     file = get_example_path("json", "parametric.json")
     dft = dftlib.io.parser.parse_dft_json_file(file)
